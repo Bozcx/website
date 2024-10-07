4e67486c1bb9fa83c048ba01f1fd1367e3de3a5f
Damian <smartypants245@gmail.com>
2024-10-06 20:57:51 -0400

    Initial upload

---
+++ src/lib/components/FirstSection.svelte
@@ -0,0 +1,32 @@
+<section class="top_section">
+	<span class="title">Hi, I'm Boz</span>
+	<span class="subtitle">This website is currently undergoing work!</span>
+</section>
+
+<style>
+    .top_section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+
+        width: 100%;
+        height: 100vh;
+
+        color: white;
+    }
+
+    .title {
+        margin: 0 15px;
+        line-height: 0.9;
+        font-size: 6rem;
+    }
+
+    .subtitle {
+        margin-bottom: 130px;
+        color: whitesmoke;
+        font-size: 1.5rem;
+        font-style: italic;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import FirstSection from '$lib/components/FirstSection.svelte';
+	import '$lib/index.css';
+</script>
+
+<section class="container">
+	<FirstSection />
+</section>
+
+<style>
+	.container {
+      background-image: url("/img/gridbackground.png");
+	}
+</style>
